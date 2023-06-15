4ea1662fdd312c9a21c7750e063f3b389161ed98
Thongek Wogponom <thongek.wpm@gmail.com>
2023-06-15 11:12:48 +0700

    commit

--- lib/models/profiles.dart
+++ lib/models/profiles.dart
@@ -3,11 +3,13 @@ class Profile {
   late String email;
   late String password;
   late String birthdate;
+  late String confirmPassword;
 
   Profile({
     required this.fullname,
     required this.email,
     required this.password,
     required this.birthdate,
+    required this.confirmPassword,
   });
 }
